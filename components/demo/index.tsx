ea1babf3bff2cca7cf14234c62b9b3284069ac64
Roaring03 <Roaring0628@gmail.com>
2024-01-06 19:01:07 +0000

    final changes added

--- components/demo/index.tsx
+++ components/demo/index.tsx
@@ -19,15 +19,15 @@ export const Demo = () => {
 
   return (
     <>
-    <div id="demo" className="w-full pt-[20px]  flex ">
-    <div className='flex-cols w-[40%]'>
-        <div className='text-white  text-start font-normal space-y-8 mt-4 px-4'>
+    <div id="demo" className="w-full pt-[20px]  flex bg-[#09090B]">
+    <div className='flex-cols w-[40%] space-y-12'>
+        <div className='text-white  text-start font-normal space-y-10 mt-4 px-40'>
         <h1 className='text-6xl text-start text-[#27272A] font-semibold '>Aegis <br/>
         <span className='text-white'>dApp</span> demo
         </h1>
-        <p className='text-[#71717A]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, repellendus iste ut soluta natus similique, veniam rem voluptas delectus odio vitae, culpa quisquam!,natus similique, veniam rem voluptas delectus odio vitae, culpa quisquam!,natus similique, veniam rem voluptas delectus odio vitae, culpa quisquam!</p>
+        <p className='text-[#71717A] line-break'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, repellendus iste ut soluta natus similique, veniam rem voluptas  Lorem ipsum dolor sit, amet consectetur adi Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, repellendus iste ut soluta natus similique, veniam rem voluptaspisicing elit. Recusandae, repellendus iste ut soluta natus similique, veniam rem voluptas delectus odio vitae, culpa quisquam!,natus similique, veniam rem voluptas delectus odio vitae, culpa quisquam!,natus similique, veniam rem voluptas delectus odio vitae, culpa quisquam!</p>
         </div>
-        <div className='flex flex-cols-2'>
+        <div className='grid grid-cols-2 px-40 mt-4'>
             {
             demoImages.map((demo, index) => (
                 <div key={index} className='flex justify-center items-center mt-4 '>
@@ -38,7 +38,7 @@ export const Demo = () => {
         </div>
     </div>
     <div className='w-[60%]'>
-        <Image src="/demoImage1.png" alt="demo" width={1920} height={1080} className='object-cover' />
+        <Image src="/demoImage.png" alt="demo" width={1920} height={1080} className='object-cover' />
     </div>
        
     </div>
--- components/partners/index.tsx
+++ components/partners/index.tsx
@@ -41,16 +41,16 @@ export const Partners = () => {
     ]
   return (
     <>
-    <div className='w-full mx-4 justify-center items-center space-y-8'>
-        <h1 className='text-6xl text-center text-white font-black '>Partners</h1>
-        <div className='flex justify-center items-center space-x-[64px] mt-8 border-1 border-t-1 border-b-1 border-neutral-700'>
+    <div className='w-full mx-4 justify-center items-center space-y-12 '>
+        <h1 className='text-6xl text-center text-white font-black mt-4'>Partners</h1>
+        <div className='flex justify-center items-center space-x-[64px] mt-8 border-1 border-t-[1px] border-b-[1px] border-neutral-700 py-3 mx-20 mb-4'>
             {
                 partners.map((partner, index) => (
                     <div key={index} className='flex flex-col items-center justify-center space-y-4 mt-12 '>
                         <Image src={partner.logo} alt={partner.name} width={96} height={96} className=' object-contain rounded-full' />
                         <div className='inline-flex space-x-1'><svg xmlns="http://www.w3.org/2000/svg" className='mt-1 p-0.5' width="19" height="18" viewBox="0 0 19 18" fill="none">
 <path d="M10.9558 7.794L16.6517 1.125H15.3017L10.3573 6.91537L6.40635 1.125H1.8501L7.82385 9.882L1.8501 16.875H3.2001L8.42235 10.7595L12.595 16.875H17.1512L10.9558 7.794ZM9.10747 9.9585L8.50222 9.08662L3.6861 2.14875H5.75947L9.64522 7.74788L10.2505 8.61975L15.3028 15.8985H13.2295L9.10747 9.9585Z" fill="white"/>
-</svg> <h1 className='text-xl text-white font-medium'>{partner.name}</h1></div>
+</svg> <h1 className='text-xl text-[#71717A] font-medium hover:text-white '>{partner.name}</h1></div>
                     </div>
                 ))
             }
