c0996b6aa2a6b4db95d7b2c51405cf41e6b6e69b
Roaring03 <Roaring0628@gmail.com>
2024-01-09 03:01:02 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -14,16 +14,27 @@ import Demo from "@/components/demo";
 import Partners from "@/components/partners";
 import { MobileNav } from "@/components/partners/mobile-nav";
 import Roadmap from "@/components/roadmap";
-import { useState } from "react";
+import { isViewportValid } from "@/utils/mediaQuery";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [isNavOpen, setIsNavOpen] = useState(false);
-  return (
+  const isMobile = isViewportValid(700);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 12720);
+  }, []);
+  return isMobile ? (
     <main className="flex min-h-screen flex-col bg-black relative">
       {/* // TODO - Add your content here */}
       {/* <EntranceGate /> */}
       <NavbarSection isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
-      {isNavOpen && <MobileNav isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />}
+      {isNavOpen && (
+        <MobileNav isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+      )}
       <HomeSection />
       <AboutSection />
       <PillarsSection />
@@ -31,8 +42,37 @@ export default function Home() {
       <Demo />
       <Roadmap />
       <Partners />
-      <FAQSection /> 
-      <FooterSection /> 
+      <FAQSection />
+      <FooterSection />
+    </main>
+  ) : isLoading ? (
+    <Preloader />
+  ) : (
+    <main className="flex min-h-screen flex-col bg-black relative">
+      {/* // TODO - Add your content here */}
+      {/* <EntranceGate /> */}
+      <NavbarSection isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+      {isNavOpen && (
+        <MobileNav isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+      )}
+      <HomeSection />
+      <AboutSection />
+      <PillarsSection />
+      <ExploreSection />
+      <Demo />
+      <Roadmap />
+      <Partners />
+      <FAQSection />
+      <FooterSection />
     </main>
   );
 }
+
+const Preloader = () => (
+  <div className="absolute max-md:hidden top-0 left-0 w-full h-full z-[999]">
+    <video autoPlay loop muted className="w-full h-full object-cover">
+      <source src="/preloader.mkv" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  </div>
+);
--- components/provider.tsx
+++ components/provider.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Lenis from "@studio-freight/lenis";
-import { isViewportValid } from "@/utils/mediaQuery";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
-  const isMobile = isViewportValid(700);
   useEffect(() => {
     const lenis = new Lenis();
 
@@ -16,31 +14,8 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
 
     requestAnimationFrame(raf);
   }, []);
-  const [isLoading, setIsLoading] = useState(true);
 
-  React.useEffect(() => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 12720);
-  }, []);
-
-  return isMobile ? (
-    <div>{children}</div>
-  ) : isLoading ? (
-    <Preloader />
-  ) : (
-    <div>{children}</div>
-  );
+  return <div>{children}</div>;
 };
 
 export { Provider };
-
-const Preloader = () => (
-  <div className="absolute max-md:hidden top-0 left-0 w-full h-full z-[999]">
-    <video autoPlay loop muted className="w-full h-full object-cover">
-      <source src="/preloader.mkv" type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
-  </div>
-);
