fbb419a623b299fac30183e49b3eb59e66f12276
Roaring03 <Roaring0628@gmail.com>
2024-01-29 22:48:55 +0530

    feat: make pdf responsive

--- app/wp.pdf/page.tsx
+++ app/wp.pdf/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import { pdfjs } from "react-pdf";
 import { Document, Page } from "react-pdf";
 import { Hourglass } from "react-loader-spinner";
+import useResizeObserver from "use-resize-observer";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
@@ -15,38 +16,53 @@ type Props = {};
 const WhitePaper = (props: Props) => {
   const [numPages, setNumPages] = useState<number>();
   const [pageNumber, setPageNumber] = useState<number>(1);
-  const [loading, setLoading] = useState<boolean>(true);
-
+  const { ref, width = 1, height = 1 } = useResizeObserver<HTMLDivElement>();
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     setNumPages(numPages);
-    setLoading(false);
   }
   return (
-    <div className={`${loading ? "h-screen overflow-hidden" : ""}`}>
-      {loading && (
-        <div className="flex items-center justify-center w-full h-screen">
-          <Hourglass
-            visible={true}
-            height="80"
-            width="80"
-            ariaLabel="hourglass-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
-            colors={["#ffffff", "#0b0b0b"]}
-          />
-        </div>
-      )}
-
-      <Document file="/whitepaper.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+    <div ref={ref}>
+      <Document
+        renderMode="canvas"
+        loading={
+          <div className="flex items-center justify-center w-full h-screen">
+            <Hourglass
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="hourglass-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              colors={["#ffffff", "#0b0b0b"]}
+            />
+          </div>
+        }
+        file="/whitepaper.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
         {Array.from({ length: 32 }, (_, index) => index + 1)?.map((page, i) => (
-          <div key={i} className="h-screen overflow-hidden">
-            <Page pageNumber={page} />
+          <div key={i} className={`h-[240px] md:h-screen overflow-hidden`}>
+            <Page
+              loading={
+                <div className="flex items-center justify-center w-full h-screen">
+                  <Hourglass
+                    visible={true}
+                    height="80"
+                    width="80"
+                    ariaLabel="hourglass-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    colors={["#ffffff", "#0b0b0b"]}
+                  />
+                </div>
+              }
+              width={width || undefined}
+              pageNumber={page}
+              renderAnnotationLayer={false}
+            />
           </div>
         ))}
       </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
     </div>
   );
 };
